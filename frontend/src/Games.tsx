118e3ead5eff84ef5f503d1577d31a443ff9c687
Akseli Vartiainen <vartiainen.akseli736@gmail.com>
2024-06-17 17:07:10 +0300

    added dropdown for teams

--- frontend/src/Games.tsx
+++ frontend/src/Games.tsx
@@ -14,9 +14,13 @@ interface Game {
 
 function Games(): ReactElement | null {
     const [games, setGames] = useState<Game[]>([]);
-    const [chosenWeek, setChosenWeek] = useState<string>("All")
-    const weeks = ["All", "1", "2", "3", "4"]
-    const defaultOption = weeks[0]
+   
+    const [chosenWeek, setChosenWeek] = useState<string>("All");
+    const weeks = ["All", "1", "2", "3", "4"];
+
+    const [chosenTeam, setChosenTeam] = useState<string>("All");
+    const teams = ["All", "Roosters", "Wolverines", "Royals", "Steelers", "Crocodiles", "Crusaders"]
+    
     const url: string = "http://localhost:3001";
 
     useEffect(() => {
@@ -28,25 +32,50 @@ function Games(): ReactElement | null {
         })
     }, []);
 
+    const handleWeekChange = (option: any) => {
+        setChosenWeek(option.value);
+    }
+
+    const handleTeamChange = (option: any) => {
+        setChosenTeam(option.value);
+    }
+
+    const filteredGames = games.filter(game => {
+        return (chosenWeek === "All" || game.week === chosenWeek) &&
+                (chosenTeam === "All" || game.home_team === chosenTeam || game.away_team === chosenTeam)
+    })
+
     return (
         <>
             <h1>Games</h1>
-            <Dropdown 
-                options={weeks} 
-                onChange={(option) => setChosenWeek(option.value)} 
-                value={defaultOption} 
-                placeholder="Select a week" 
-            />
+            <div className='dropdown-container'>
+                <div className='dropdown-wrapper'>
+                    <p>Choose a week</p>
+                        <Dropdown 
+                            options={weeks} 
+                            onChange={handleWeekChange} 
+                            value={chosenWeek} 
+                            placeholder="Select a week" 
+                        />
+                </div>
+                <div className='dropdown-wrapper'>
+                    <p>Choose a team</p>
+                    <Dropdown 
+                        options={teams} 
+                        onChange={handleTeamChange} 
+                        value={chosenTeam} 
+                        placeholder="Select a team" 
+                    />
+                </div>
+            </div>
             <p>{!games ? "API might not be working" : null}</p>
-            {games && 
-                games
-                    .filter(game => game.week === chosenWeek || chosenWeek === "All")
-                    .map((game) => (
-                        <div key={game._id}>
-                            <p>Week : {game.week} - {game.home_team} {game.home_score} - {game.away_score} {game.away_team}</p>
-                        </div>
-                    ))
-            }
+            {filteredGames.length > 0 ?
+                filteredGames.map((game) => (
+                    <div key={game._id}>
+                         <p>Week {game.week} : {game.home_team} {game.home_score} - {game.away_score} {game.away_team}</p>
+                    </div>
+                ))
+            : <p>No games available for the selected filters</p>}
         </>
     )
 }
